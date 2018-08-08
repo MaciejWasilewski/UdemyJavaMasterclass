0869232d15ec4edd0d89f1abdc38ef4aa024d132
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2018-08-08 17:56:57 +0200

    up to82

---
+++ ListAndArrayList/src/com/mw/GroceryList.java
@@ -0,0 +1,30 @@
+package com.mw;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+
+    }
+
+    public void printGroceryList() {
+        System.out.println("Size of list: "+groceryList.size());
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int pos, String newItem) {
+        groceryList.set(pos, newItem);
+        System.out.println("Item " + pos + " has been modified.");
+    }
+
+    public void removeGroceryItem(int pos) {
+        String theItem = groceryList.get(pos);
+        groceryList.remove(pos);
+        System.out.println("Position " + theItem + " removed.");
+    }
+}
---
+++ ListAndArrayList/src/com/mw/Main.java
@@ -0,0 +1,13 @@
+package com.mw;
+
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
