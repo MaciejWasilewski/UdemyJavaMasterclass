69c2c46e55a5d6a08fd8998f838f0e842095cdb6
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2019-02-08 16:32:53 +0100

    on 135

--- 130CollectionsOverview/src/com/example/MapProgram.java
+++ 130CollectionsOverview/src/com/example/MapProgram.java
@@ -8,5 +8,13 @@ public static void main(String[] args){
     Map<String,String> languages=new HashMap<>();
     languages.put("Java","java language");
     languages.put("python", "python is a programming language");
+//    System.out.println(languages.get("python"));
+//    System.out.println(languages.get("Python"));
+    for(String s: languages.keySet())
+    {
+        System.out.println(languages.get(s));
+    }
+    languages.remove("Java");
+
 }
 }
---
+++ 135MapAndGame/src/com/example/Main.java
@@ -0,0 +1,71 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    private static Map<Integer, Location> locations = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        locations.put(0, new Location(0, "In front of the computer"));
+        locations.put(1, new Location(1, "Before small brick building"));
+        locations.put(2, new Location(2, "Top of the hill"));
+        locations.put(3, new Location(3, "Inside building"));
+        locations.put(4, new Location(4, "In a valley"));
+        locations.put(5, new Location(5, "In the forest"));
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("Q", 0);
+
+        locations.get(2).addExit("N", 5);
+        locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+        locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+        locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+        locations.get(5).addExit("Q", 0);
+
+        int loc = 1;
+        String locCandidate;
+        while (true) {
+            System.out.println(locations.get(loc).getDescription());
+            System.out.println(locations.get(loc).getExits());
+            if (loc == 0) {
+                break;
+            }
+            while (true) {
+                if (scanner.hasNext()) {
+                    locCandidate = scanner.next();
+                    if (locCandidate.equals("W") || locCandidate.equals("E") || locCandidate.equals("S") || locCandidate.equals("N") || locCandidate.equals("Q")) {
+                        break;
+                    } else {
+                        System.out.println("You should enter a direction (WESN)!");
+                    }
+                } else {
+                    System.out.println("You should enter a direction (WESN)!");
+                }
+            }
+
+            if (!locations.get(loc).getExits().containsKey(locCandidate)) {
+                System.out.println("You cannot go in that direction");
+
+            } else {
+                loc = locations.get(loc).getExits().get(locCandidate);
+            }
+        }
+
+    }
+}
