9f44d51a15c4f703eddf818204541c75e97db4e7
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2019-07-22 17:12:55 +0200

    263

--- 263Threads/src/com/example/mwasilewski/Main.java
+++ 263Threads/src/com/example/mwasilewski/Main.java
@@ -3,6 +3,23 @@ package com.example.mwasilewski;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello from the main thread");
+        Thread anotherThread=new AnotherThread();
+        anotherThread.start();
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                System.out.println("Hello from the second thread");
+            }
+        };
+        thread.start();
+        new Thread(){
+            @Override
+            public void run() {
+                System.out.println("Hello from second anonymous thread");
+            }
+        }.start();
+        new Thread(() -> System.out.println("Hello from third anonymous thread")).start();
+        System.out.println("Hello again from the main thread");
     }
 }
