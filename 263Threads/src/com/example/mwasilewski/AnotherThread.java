efa60bfcfa011166048f4b200f8572da42f0f1c1
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2019-07-23 18:25:35 +0200

    269

--- 263Threads/src/com/example/mwasilewski/AnotherThread.java
+++ 263Threads/src/com/example/mwasilewski/AnotherThread.java
@@ -3,6 +3,17 @@ package com.example.mwasilewski;
 public class AnotherThread extends Thread {
     @Override
     public void run() {
-        System.out.println("Hello from another thread");
+        System.out.println("Hello from another thread "+currentThread().getName());
+        try{
+            Thread.sleep(3000);
+            System.out.println("Three seconds have passed");
+
+        } catch (InterruptedException e) {
+            System.out.println("Another thread woke me up!");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Outside sleep block");
+
     }
 }
--- 263Threads/src/com/example/mwasilewski/Main.java
+++ 263Threads/src/com/example/mwasilewski/Main.java
@@ -6,20 +6,26 @@ public class Main {
         System.out.println("Hello from the main thread");
         Thread anotherThread=new AnotherThread();
         anotherThread.start();
-        Thread thread=new Thread(){
-            @Override
-            public void run() {
-                System.out.println("Hello from the second thread");
-            }
-        };
+//        anotherThread.interrupt();
+//        anotherThread=new AnotherThread();
+//        anotherThread.run();
+        Thread thread= new Thread(() -> System.out.println("Hello from the second thread"));
+        try {
+            anotherThread.join(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         thread.start();
-        new Thread(){
-            @Override
-            public void run() {
-                System.out.println("Hello from second anonymous thread");
-            }
-        }.start();
+        new Thread(() -> System.out.println("Hello from second anonymous thread")).start();
         new Thread(() -> System.out.println("Hello from third anonymous thread")).start();
         System.out.println("Hello again from the main thread");
+        new Thread(new MyRunnable()).start();
+
+        new Thread(new MyRunnable(){
+            @Override
+            public void run() {
+                System.out.println("Hello from anonymous extension to MyRunnable!");
+            }
+        }).start();
     }
 }
