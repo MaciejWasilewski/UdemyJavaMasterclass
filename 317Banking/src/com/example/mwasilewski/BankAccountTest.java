9ca7d06e00297df4b079796b7a2b813c1c3a7746
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2019-08-09 18:14:19 +0200

    319

---
+++ 317Banking/src/com/example/mwasilewski/BankAccountTest.java
@@ -0,0 +1,58 @@
+package com.example.mwasilewski;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+    private BankAccount account;
+
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setup() {
+        account = new BankAccount("a", "b", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        assertEquals(1200, account.deposit(200, true), 0);
+        assertEquals(1200, account.getBalance(), 0);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw_notBranch() {
+        assertThrows(IllegalArgumentException.class,()->account.withdraw(600,false));
+    }
+    @org.junit.jupiter.api.Test
+    void withdraw_branch() {
+        assertEquals(400,account.withdraw(600,true));
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_deposit() {
+
+        account.deposit(200, true);
+        assertEquals(1200, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance_withdraw() {
+
+        account.withdraw(200, true);
+        assertEquals(800, account.getBalance(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void isChecking_true() {
+
+        assertTrue(account.isChecking(), "The account is not, but should be, checking account");
+    }
+
+
+
+
+}
---
+++ 317Banking/src/com/example/mwasilewski/Main.java
@@ -0,0 +1,8 @@
+package com.example.mwasilewski;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This app is running");
+    }
+}
