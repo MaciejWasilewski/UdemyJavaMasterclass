fca0a3234a8365adc8c00f51a68f67983bcc2441
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2019-08-13 16:46:00 +0200

    332

--- 321JUnitChallenge/src/com/mw/UtilitiesTest.java
+++ 321JUnitChallenge/src/com/mw/UtilitiesTest.java
@@ -1,7 +1,8 @@
 package com.mw;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.util.Random;
 
@@ -9,13 +10,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UtilitiesTest {
 
+
     @Test
     void everyNthChar() {
        assertArrayEquals(new char[]{'e','l'}, Utilities.everyNthChar(new char[]{'h','e','l','l','o'},2));
         assertArrayEquals(new char[]{'h','e','l','l','o'}, Utilities.everyNthChar(new char[]{'h','e','l','l','o'},6));
+
     }
 
-    @Test
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "dataForTest.txt", numLinesToSkip = 1)
+    void testRemovePairs(String input, String output)
+    {
+        assertEquals(output,Utilities.removePairs(input));
+    }
+
     void removePairs() {
         Random random=new Random();
         assertEquals("abcde",Utilities.removePairs("aabccdee"));
@@ -28,7 +39,7 @@ class UtilitiesTest {
     @Test
     void testConverter() {
         assertEquals(300,Utilities.converter(10,5));
-
+        assertThrows(ArithmeticException.class,()->Utilities.converter(10,0));
     }
 
     @Test
