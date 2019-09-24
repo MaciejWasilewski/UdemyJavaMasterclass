076d2a6b20536b490a77ba7a0208263d431ced6b
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2019-09-24 17:55:44 +0200

    370

--- 364Networking/src/com/example/mwasilewski/Main.java
+++ 364Networking/src/com/example/mwasilewski/Main.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -30,7 +35,7 @@ public class Main {
             //            System.out.println(baseUri.resolve(uri3).toURL());
             //            //relativization
             //            System.out.println(baseUri.relativize(baseUri.resolve(uri2)));
-            URL url = new URL("https://example.org");
+            URL url = new URL("https://www.flickr.com/services/feeds/photos_public.gne?tags=large-format");
             //            URI uri = url.toURI();
             //            System.out.println("scheme = " + uri.getScheme());
             //            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
@@ -48,17 +53,37 @@ public class Main {
             //            } catch (IOException e) {
             //                e.printStackTrace();
             //            }
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            //            urlConnection.setDoOutput(true);
+            //            urlConnection.connect();
+            //            try (BufferedReader inputStream =
+            //                         new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+            //                Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+            //                headerFields.entrySet().stream().map(e -> "Header: "+e.getKey() + e.getValue().stream()
+            //                .reduce(
+            //                        "\n\tField" +
+            //                                ": ",
+            //                        (i,s)->i.concat(s+" * "))).forEach(System.out::println);
+            //                //                inputStream.lines().forEach(System.out::println);
+            //            } catch (IOException e) {
+            //                e.printStackTrace();
+            //            }
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("User-Agent", "Chrome");
+            int responseCode = urlConnection.getResponseCode();
+            System.out.println(responseCode);
+            urlConnection.setReadTimeout(30000);
+            Pattern p = Pattern.compile("<link rel.+?href=\"(.+?\\.jpg)\"");
+            //            Matcher m=p.matcher("<link rel=\"enclosure\" " +
+            //                    "type=\"image/jpeg\" " +
+            //                    "href=\"https://live.staticflickr.com/65535/48788081337_b18fcca3df_b.jpg\" />");
+            //            m.matches()
             try (BufferedReader inputStream =
                          new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
-                inputStream.lines().forEach(System.out::println);
+                inputStream.lines().map(p::matcher).filter(Matcher::find).forEach(m -> System.out.println(m.group(1)));
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
